32d93bb2b71265782b7c861516efd51bf7401fda
engineer-avinash <csengineer.avinash@gmail.com>
2020-06-11 17:54:14 +0530

    Updates

---
+++ precache-manifest.12eb77b9b4d66f0f4491cb16b2493f93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d09ae2ee684021f09ca8ab67a9e115",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "8070e6cafe82fd669b88",
+    "url": "/quiz-app/static/css/main.f1ff90b6.chunk.css"
+  },
+  {
+    "revision": "e0635992ccd388b542e8",
+    "url": "/quiz-app/static/js/2.5015fd63.chunk.js"
+  },
+  {
+    "revision": "8070e6cafe82fd669b88",
+    "url": "/quiz-app/static/js/main.60255622.chunk.js"
+  },
+  {
+    "revision": "929021f0506d4ac5f2ec",
+    "url": "/quiz-app/static/js/runtime~main.9947e4ab.js"
+  }
+]);
